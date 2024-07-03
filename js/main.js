7677aab5d9aff7b89ee129e9bee6fc8bb5d92f6b
Bruno Fanini <phoenixbf@gmail.com>
2024-07-03 16:23:56 +0200

    spaces

--- js/main.js
+++ js/main.js
@@ -16,6 +16,10 @@ APP.setup = ()=>{
 
     ATON.FE.realize(); // Realize the base front-end
 	ATON.FE.addBasicLoaderEvents(); // Add basic events handling
+
+    APP.setupEvents();
+
+    APP.loadConfig();
 };
 
 // Config
@@ -32,10 +36,36 @@ APP.loadConfig = ()=>{
 
 // Events
 //========================================================
-ATON.on("APP_ConfigLoaded", ()=>{
+APP.setupEvents = ()=>{
+    ATON.on("APP_ConfigLoaded", ()=>{
+        let space = APP.params.get("s");
+        if (space) APP.loadSpace(space);
+    });
+    
+    ATON.on("APP_SpaceLoaded", (spaceid)=>{
+        console.log("Space: '"+spaceid+"' loaded.");
+    
+        //ATON.Photon.connect();
+    });
+};
 
-});
 
+// Spaces
+//========================================================
+APP.loadSpace = (spaceid)=>{
+    let S = APP.confdata.spaces[spaceid];
+    if (!S) return;
+
+    let sid = S.sid;
+    if (!sid) return;
+
+    ATON.FE.loadSceneID( sid, ()=>{
+        ATON.fireEvent("APP_SpaceLoaded",spaceid);
+    });
+
+    let portals = S.portals;
+    // realize SUI portals
+};
 
 
 // Update
