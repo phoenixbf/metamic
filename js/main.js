ce528e1d1b15ff75019dbcb6f23cfe267286408c
Bruno Fanini <phoenixbf@gmail.com>
2024-12-09 22:22:18 +0100

    huge update

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 import Portal from "./portal.js";
 
 let APP = ATON.App.realize();
+//APP.requireFlares(["myFlare"]);
 window.APP = APP;
 
 APP.pathConfigFile   = APP.basePath + "config.json";
@@ -21,6 +22,8 @@ APP.setup = ()=>{
     APP._mode = APP.MODE_INSPECTION;
     
     APP._currSpaceID = undefined;
+    APP._layers = [];
+    APP._currLayer = 0;
 
     ATON.FE.realize(); // Realize the base front-end
 	ATON.FE.addBasicLoaderEvents(); // Add basic events handling
@@ -31,6 +34,18 @@ APP.setup = ()=>{
 
     APP.gPortals = ATON.createUINode();
     APP.gPortals.attachToRoot();
+
+    //ATON._mainRoot.background = ATON.MatHub.colors.white;
+    //ATON._mainRoot.fog = new THREE.FogExp2( new THREE.Color( 0.9,0.9,0.9 ), 0.08 );
+    //ATON._renderer.shadowMap.enabled = true;
+
+    APP._CSM = undefined;
+
+    // If our app required ore or more flares (plugins), we can also wait for them to be ready for specific setups
+    ATON.on("AllFlaresReady",()=>{
+		// Do stuff
+		console.log("All flares ready");
+	});
 };
 
 APP.setMode = (m)=>{
@@ -49,6 +64,23 @@ APP.loadConfig = ()=>{
     });
 };
 
+APP.setupShadows = ()=>{
+    let S = ATON._dMainL.shadow;
+    if (!S) return;
+
+    S.camera.left   = -50;
+    S.camera.right  = 50;
+    S.camera.bottom = -50;
+    S.camera.top    = 50;
+
+    S.mapSize.width  = 2048;
+    S.mapSize.height = 2048;
+    
+    S.camera.near    = 0.1;
+    S.camera.far     = 150;
+    S.bias           = 0.0; //-0.0005;
+};
+
 // Events
 //========================================================
 APP.setupEvents = ()=>{
@@ -57,6 +89,58 @@ APP.setupEvents = ()=>{
         let portal = APP.params.get("p");
 
         if (space) APP.loadSpace(space, portal);
+    });
+
+    ATON.on("AllNodeRequestsCompleted", ()=>{
+        console.log(APP._layers);
+
+        // CSM
+/*
+        APP._CSM = new THREE.CSM({
+            maxFar: 200,
+            cascades: 4,
+            //mode: params.mode,
+            parent: ATON._rootVisible,
+            shadowMapSize: 1024,
+            lightDirection: ATON._dMainLdir,
+            camera: ATON.Nav._camera
+        });
+*/
+        let sc = APP.confdata.spaces[APP._currSpaceID];
+
+        let M = ATON.getSceneNode("main");
+
+        // custom setup shadows
+        APP.setupShadows();
+/*
+        M.traverse( ( o ) => {
+            o.castShadow = true;
+            o.receiveShadow = true;
+
+            if (o.material){
+                APP._CSM.setupMaterial( o.material );
+                o.material.needsUpdate = true;
+            }
+        });
+*/
+        let numlayers = APP._layers.length;
+
+        for (let i=1; i<numlayers; i++){
+            let L = ATON.getSceneNode(APP._layers[i]);
+            
+            //L.hide();
+/*
+            let axis = i%3;
+
+            if (axis===0) L.scale.x = 0.01;
+            if (axis===1) L.scale.y = 0.01;
+            if (axis===2) L.scale.z = 0.01;
+*/
+            L.scale.y = 0.05;
+            
+            //L.position.y = (((numlayers-i) * 0.1) / M.scale.x);
+            L.position.y = ((i * -0.15) / M.scale.x);
+        }
     });
     
     ATON.on("APP_SpaceEnter", (spaceid)=>{
@@ -69,6 +153,16 @@ APP.setupEvents = ()=>{
 
     ATON.on("APP_PortalEnterRequest", d => {
         APP.loadSpace(d.space, d.portal);
+    });
+
+    // Keyb
+	ATON.on("KeyPress", k =>{
+		if (k==='u'){
+            ATON.updateLightProbes();
+		}
+		if (k==='a'){
+            APP.requestNextLayerAnimation();
+		}
     });
 };
 
@@ -87,6 +181,8 @@ APP.loadSpace = (spaceid, portalid)=>{
 
     ATON.FE.loadSceneID( sid, ()=>{
         APP._currSpaceID = spaceid;
+
+        APP._layers = S.modules;
 
         if (portalid && S.portals[portalid]){
             let P = S.portals[portalid];
@@ -141,7 +237,42 @@ APP.clearPortals = ()=>{
 
 // Update
 //========================================================
+APP.requestLayerAnimation = (layerid)=>{
+    if (APP._reqLayer !== undefined) return;
+
+    APP._reqLayer = ATON.getSceneNode(layerid);
+};
+
+APP.requestNextLayerAnimation = ()=>{
+    if (APP._currLayer >= APP._layers.length) return;
+
+    APP._currLayer += 1;
+
+    APP.requestLayerAnimation( APP._layers[APP._currLayer] );
+};
+
+APP.handleLayerAnimation = ()=>{
+    if (!APP._reqLayer) return;
+
+    APP._reqLayer.scale.y += 0.05;
+    APP._reqLayer.position.y *= 0.8;
+
+    if (APP._reqLayer.scale.y > 1.0){
+        APP._reqLayer.scale.x = 1.0;
+        APP._reqLayer.scale.y = 1.0;
+        APP._reqLayer.scale.z = 1.0;
+
+        APP._reqLayer.position.y = 0.0;
+        
+        APP._reqLayer = undefined;
+
+        ATON.updateLightProbes();
+    }
+};
+
 APP.update = ()=>{
+    APP.handleLayerAnimation();
+    if (APP._CSM) APP._CSM.update();
 };
 
 
